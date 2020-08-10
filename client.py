68c10ed2aaec35f2247193f22215da01c2442a7f
connor <connormullet@gmail.com>
2020-08-10 08:52:55 -0400

    added todo log, changed client.py for testing

--- client.py
+++ client.py
@@ -6,25 +6,47 @@ import time
 HOST = '127.0.0.1'
 PORT = 5250
 
-set_request = b'set\tfoo\tbar\n'
-get_request = b'get\tfoo\n'
-del_request = b'del\tfoo\n'
+
+def parse_command(command):
+    command = command.split()
+    out = ""
+
+    # get and del
+    if command[0] == "get" or command[0] == "del":
+        out = f"{command[0]}\t{command[1]}\n"
+    # set
+    elif command[0] == "set":
+        value = " ".join(command[2:])
+        out = f"{command[0]}\t{command[1]}\t{value}\n"
+    else:
+        print("Invalid arguments")
+        exit()
+    return out
 
 
 def make_connection(request):
+    request = request.encode()
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
         s.sendall(request)
         data = s.recv(1024)
-    return data
+    return data.decode('utf-8')
 
-set_data = make_connection(set_request)
 
-assert set_data.decode('utf-8') == "OK"
+def main():
 
-get_data = make_connection(get_request)
-assert get_data.decode('utf-8') == "bar"
+    while 1:
+        command = input('> ')
 
-del_data = make_connection(del_request)
-assert del_data.decode('utf-8') == "OK"
+        if command == "quit":
+            exit()
+
+        request = parse_command(command)
+        data = make_connection(request)
+
+        print(data)
+
+
+if __name__ == '__main__':
+    main()
---
+++ todo.md
@@ -0,0 +1,11 @@
+
+# TODO
+
+- Server CLI (accessible over TCP and locally via terminal)
+- More data types
+- command line configuration (server and client)
+- `dmp` command for dumping table content to client
+- daemonize
+- timeout for longer connections
+- install script to create systemd service
+
